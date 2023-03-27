56b44bf18e231919652c44efa31804a0a517b033
Максим Моисеев <85686839+Drfarkos@users.noreply.github.com>
2023-03-27 19:21:02 +0000

    debug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: '0.0.0.0',
-  }
-})
+    fs: {
+      strict: false,
+    },
+  },
+});
